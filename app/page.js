f6baca76acc0f34bbca58fa0075633d014a9d4f5
wordfan18 <105841105322@student.unismuh.ac.id>
2024-08-14 15:19:25 +0800

    chapter 4 selesai

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/hero-mobile.png"
             width={560}
             height={620}
             className="hidden md:block"
